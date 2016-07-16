f2225f750692689cfb80c49d2fc2d90dc7848672
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-07-16 14:48:09 +0200

    add put todos

--- server.js
+++ server.js
@@ -58,6 +58,34 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+app.put('/todos/:id', function (req, res) {
+    const todoId = parseInt(req.params.id, 10);
+    var body = req.body;
+    body = _.pick(body, 'description', 'completed');
+    var validAttributes = {};
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+
+    res.json(matchedTodo);
+});
+
 app.listen(PORT, function () {
     console.log('Express listening on port: ' + PORT);
 });
