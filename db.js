1cbb1333af1dd4409c662b2866d5b0373a32e90d
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-07-17 11:12:49 +0200

    add postgress for production

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 const Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
+const env = process.env.NODE_ENV || 'development';
+
+var sequelize;
+if (env === 'production') {
+    // running on Heroku
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialiect: 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',
     'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+    });
+}
 
 var db = {};
 
