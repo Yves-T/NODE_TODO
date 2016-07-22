43a306f01f1b8209e65f05562b1b54c89be07acc
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-07-22 07:26:23 +0200

    Move cursor at end of text after input focus

--- public/ng/directives/focus.js
+++ public/ng/directives/focus.js
@@ -7,11 +7,35 @@
             'use strict';
 
             return function (scope, elem, attrs) {
+
+                function setCaretAtEnd(elem) {
+                    var elementLength = elem.value.length;
+                    // For IE Only
+                    if (document.selection) {
+                        // Set focus
+                        elem.focus();
+                        // Use IE Ranges
+                        var objectSelection = document.selection.createRange();
+                        // Reset position to 0 & then set at end
+                        objectSelection.moveStart('character', -elementLength);
+                        objectSelection.moveStart('character', elementLength);
+                        objectSelection.moveEnd('character', 0);
+                        objectSelection.select();
+                    }
+                    else if (elem.selectionStart || elem.selectionStart == '0') {
+                        // Firefox/Chrome
+                        elem.selectionStart = elementLength;
+                        elem.selectionEnd = elementLength;
+                        elem.focus();
+                    }
+                }
+
                 scope.$watch(attrs.todoFocus, function (newVal) {
+
                     if (newVal) {
                         $timeout(function () {
-                            elem[0].focus();
-                        }, 0, false);
+                            setCaretAtEnd(elem[0]);
+                        }, 50, false);
                     }
                 });
             };
